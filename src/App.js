26581dab7368feddbec3b90c3cb24dafe5b214a1
Yaroslav Semenov <yaryk2013@gmail.com>
2022-05-02 22:19:09 +0300

    rendered task 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import User from 'components/User/User';
+import Profile from './components/Profile/Profile';
 import user from './data/user.json';
+import Statistics from './components/Statistics/Statistics';
+import data from './data/statistics.json';
 
 export default function App() {
   return (
-    <User
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      followers={user.stats.followers}
-      views={user.stats.views}
-      likes={user.stats.likes}
-    ></User>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+    </>
   );
 }
